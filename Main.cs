27ec2169aa0411bad8a38db2b857a1d7e8c043e4
Noah Husby <noahhusby@gmail.com>
2018-03-19 14:49:39 -0400

    Added base classes, connecting remote mac agent

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace MachineFlow_iOS
+{
+    [Register("Main")]
+    public class Main : UIView
+    {
+        public Main()
+        {
+            Initialize();
+        }
+
+        public Main(RectangleF bounds) : base(bounds)
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            BackgroundColor = UIColor.Red;
+        }
+    }
+}
